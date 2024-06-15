70b7bbdc513f49a301f8ccf20366edf9e2967857
Sule Tunkara <9588380+st4977f@users.noreply.github.com>
2024-06-15 02:15:33 +0100

    Cors headers

--- server.js
+++ server.js
@@ -27,8 +27,9 @@ const port = process.env.PORT || 3000; // Default to port 3000 if PORT is not se
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: process.env.CORS_ALLOWED_ORIGINS,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
